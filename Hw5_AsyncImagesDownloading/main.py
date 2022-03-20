1ad9198703f891c4c123739dc757923588a649d2
Frosendroska <katyabraun0@gmail.com>
2022-03-20 12:40:27 +0300

    done async easy

--- Hw5_AsyncImagesDownloading/main.py
+++ Hw5_AsyncImagesDownloading/main.py
@@ -1,41 +1,42 @@
-import os
 import time
-import aiohttp
-import asyncio
 import os
 import asyncio
 import aiohttp  # pip install aiohttp
 import aiofiles  # pip install aiofiles
-from pip._internal.utils import urls
 
 FILES_PATH = "artifacts/easy"
-SITE = 'https://thiscatdoesnotexist.com/'
+SITE = 'https://picsum.photos/600/600?random'
+FILENAME = SITE.split("/")[-2]
 IMG_NUMBER = 20
 
-if __name__ == '__main__':
-    if not os.path.exists(FILES_PATH):
-        path = FILES_PATH.split('/')
-        for dir in path:
-            if not os.path.exists(dir):
-                os.mkdir(dir)
-            os.chdir(dir)
 
-    sema = asyncio.BoundedSemaphore(5)
-
-
-    async def fetch_file():
-        arr = SITE.split("/")
-        file_name = SITE.split("/")[-2]
-        async with sema, aiohttp.ClientSession() as session:
-            for i in range(IMG_NUMBER):
+async def downloading_task(i):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SITE) as resp:
+            async with aiofiles.open(f"{FILENAME}_{i + 1}.png", "bw") as file:
                 start = time.time()  ####
-                async with session.get(SITE) as resp:
-                    async with aiofiles.open(f"{FILES_PATH}/{file_name}_{i + 1}.png", "bw") as file:
-                        await file.write((await resp.content.read()))
-                        print(f"Time for img {i + 1}: " + str(time.time() - start))
-                time.sleep(1)
+                await file.write((await resp.read()))
+                print(f"Time for img {i + 1}: " + str(time.time() - start))  ###
 
+
+async def fetch_files():
+    tasks = []
+    for i in range(IMG_NUMBER):
+        tasks.append(asyncio.create_task(downloading_task(i)))
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    path = FILES_PATH.split('/')
+    if not os.path.exists(FILES_PATH):
+        for curdir in path:
+            if not os.path.exists(curdir):
+                os.mkdir(curdir)
+            os.chdir(curdir)
+    else:
+        for curdir in path:
+            os.chdir(curdir)
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_file())
+    loop.run_until_complete(fetch_files())
     loop.close()
